69223a75ae4766e39fab1a4bcf828d6099e2a45b
zurakalandia <zurakala2010@gmail.com>
2024-10-20 11:45:51 +0400

    second commit

---
+++ level 3/class/variables.py
@@ -0,0 +1,10 @@
+name = 'zura'
+last_name = 'kalandia'
+age = '14'
+country = 'georgia'
+city = 'tbilisi'
+favorite_car= 'porsche'
+hobby= 'basketbsll, coding'
+favorite_color = 'black'
+
+print('my name is ' + name + ' ' + last_name + '. im ' + age + '. im from ' + country + ', ' + city + '. my favorite car is ' + favorite_car + '. ' + 'i enjoy ' + hobby + '. ' + 'my favorite color is ' + favorite_color + '.') 
---
+++ level 3/home/work.py
@@ -0,0 +1,31 @@
+# to create a variable we need to give it : a name, value, and a symbol to connect them (=)
+
+# for example we give the variable a name - 'money'
+# then we give it a value - '75000'
+# and finally, connect it with (=)
+
+# example:
+
+money = (75000)  # variable
+
+
+
+num1 = (90)
+num2 = (75)
+num3 = (4)
+
+print(num1 + num2 + num3)
+
+
+
+x = (5)
+y = ('ab')
+
+print(x * y)
+
+
+
+length = (20)
+width = (10)
+
+print(length * 2 + width * 2)
