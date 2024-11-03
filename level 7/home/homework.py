c77b304861235388d3e9cbeecb641f75b3480531
zurakalandia <zurakala2010@gmail.com>
2024-11-03 10:41:43 +0400

    sixth

--- level 7/home/homework.py
+++ level 7/home/homework.py
@@ -1,7 +1,123 @@
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x + y)
 
 
 
 x = int(input('X = '))
 y = int(input('Y = '))
+print(x + y)
 
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x + y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x + y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x + y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x - y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x - y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x - y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x - y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x - y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x * y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x * y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x * y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x * y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x * y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
 print(x // y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x // y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x // y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x // y)
+
+
+
+x = int(input('X = '))
+y = int(input('Y = '))
+print(x // y)
+
+
+#int() - სტრინგი და ფლოტი რიცხვი გადაიყვანოს ინტეჯერად
+#str() - მონაცემებს გარდაქმნის სტრინგებად
+#type() - გამოიყენება მონაცემების ტიპის დასადგენად
+#input() - გამოიყენება მომხმარებლის მიერ მონაცემების შესატანად
