15d50b297a59cdefbbdee1ae552996ae239e5d93
zurakalandia <zurakala2010@gmail.com>
2025-01-18 15:51:49 +0400

    nineteenth commit

---
+++ level 27/home/codewars3.py
@@ -0,0 +1,7 @@
+# A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+# def main [verb, noun]
+# return verb + noun
+
+def main (verb, noun):
+    return verb + noun
