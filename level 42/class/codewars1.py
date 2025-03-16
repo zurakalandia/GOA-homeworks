dd233103ccbf6d9f29b4e1412d53442089317bcb
zurakalandia <zurakala2010@gmail.com>
2025-03-16 20:53:22 +0400

    twenty fifth commit

--- level 42/class/codewars1.py
+++ level 42/class/codewars1.py
@@ -5,6 +5,8 @@ He just can't stop asking the creatures he encounters what they like to eat. But
 
 When he meets the alligator, it then makes a tiny mouth.
 
+
+
 Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide.
 '''
 
