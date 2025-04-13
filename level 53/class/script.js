bbebea70844f704c5a88cf8ba38a51de3214436b
zurakalandia <zurakala2010@gmail.com>
2025-04-13 19:29:27 +0400

    level 53 commit

---
+++ level 53/class/script.js
@@ -0,0 +1,43 @@
+alert('Hello, my name is Zura');
+alert('My last name is Kalandia');
+alert('I am 14 years old');
+alert('My favorite car is Porsche');
+alert('My favorite color is either black or purple');
+
+alert(10 + 5);
+alert(20 + 49);
+alert(1 + 3);
+alert(7 + 20);
+alert(30 + 25);
+
+alert(9 - 6);
+alert(4 - 7);
+alert(150 - 25);
+alert(75 - 50);
+alert(169 - 33);
+
+alert(8 * 6);
+alert(12 * 11);
+alert(17 * 3);
+alert(9 * 1);
+alert(7 * 3);
+
+alert(49 / 7);
+alert(10 / 2);
+alert(3 / 4);
+alert(100 / 25);
+alert(25 / 5);
+
+alert(49 % 5);
+alert(90 % 7);
+alert(80 % 49);
+alert(100 % 99);
+alert(90 % 2);
+
+alert(7 ** 3);
+alert(8 ** 2);
+alert(2 ** 3);
+alert(3 ** 5);
+alert(5 ** 4);
+
+alert('JS(JavaScript) is one of the most popular programming languages in the world. its used in building websites and much more.')
