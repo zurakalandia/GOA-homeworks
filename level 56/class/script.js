4f51b08618d0667b4f333655a4afceefcdf45844
zurakalandia <zurakala2010@gmail.com>
2025-04-25 22:35:46 +0400

    level 56 commit

---
+++ level 56/class/script.js
@@ -0,0 +1,9 @@
+let name = 'Zura';
+let lName = 'Kalandia';
+let age = 14;
+let profession = 'none'
+let favLanguage = 'python'
+
+console.log('hello, my name is ' + name + ' my last name is ' + lName + '. My profession is ' + profession + ' and my favorite programmming language is ' + favLanguage);
+
+alert('hello, my name is ' + name + ' my last name is ' + lName + '. My profession is ' + profession + ' and my favorite programmming language is ' + favLanguage);
---
+++ level 56/home/script.js
@@ -0,0 +1,17 @@
+let name = 'Zura';
+
+let password = '123456789';
+
+let hobby = 'coding';
+
+const id = 939812;
+
+const color = 'purple'
+
+const char = 'i'
+
+//let is a type of a variable that gives us the ability to reassign the variable later
+
+//const is another type of a variable that stays the same and doesnt let us change it, its constant
+
+//let variables allows us to reassign it later, while the const variable doesnt allow us to do it
